c6e074fbc4eb950fd4ed744927ea737e8feb792b
Kiiiiirsty <30590953+Kiiiiirsty@users.noreply.github.com>
2017-08-03 11:08:25 +0100

    Jquery toggle mood buttons complete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-$('button.scrollsomething').on('click', function() {
-  $.smoothScroll({
-    scrollElement: $('.buttonToSelect'),
-    scrollTarget: '#moodSelection'
-  });
-  return false;
+$(document).ready(function(){
+  $("iframe").hide();
+    $("#moodSelection .happy").click(function(){
+        $(".ihappy").fadeToggle();
+    });
+
+    $("#moodSelection .relaxed").click(function(){
+        $(".irelaxed").fadeToggle();
+    });
+
+    $("#moodSelection .down").click(function(){
+        $(".idown").fadeToggle();
+    });
+
+    $("#moodSelection .anxious").click(function(){
+        $(".ianxious").fadeToggle();
+    });
+
+    $("#moodSelection .neutral").click(function(){
+        $(".ineutral").fadeToggle();
+    });
+
+
 });
